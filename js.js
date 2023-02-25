6c651ad5250f047a1e1078c9f13920ee695eae78
muhiedeen <muhiedeen.alhindi@gmail.com>
2023-02-25 18:30:09 +0200

    added event to title and button

---
+++ js.js
@@ -0,0 +1,17 @@
+window.onload=function(){
+ let signin_btn=document.getElementById("sign-in")
+ let signup_btn=document.getElementById("sign-up")
+ let title=document.getElementById("title")
+ 
+
+ signin_btn.addEventListener("click",function(){
+
+    title.innerHTML="sign in"
+    })
+signup_btn.addEventListener("click",function(){
+
+        title.innerHTML="sign up"
+        })
+
+
+}
