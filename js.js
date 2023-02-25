a47c3166dc18e9eb2a54f2d640bf588c3417966d
muhiedeen <muhiedeen.alhindi@gmail.com>
2023-02-25 18:57:40 +0200

    made  some form to hide during sign in

--- js.js
+++ js.js
@@ -2,15 +2,23 @@ window.onload=function(){
  let signin_btn=document.getElementById("sign-in")
  let signup_btn=document.getElementById("sign-up")
  let title=document.getElementById("title")
+ let div_name=document.getElementById("reg-form")
+ let div_user=document.getElementById("username")
+ let div_date=document.getElementById("date")
  
 
  signin_btn.addEventListener("click",function(){
 
     title.innerHTML="sign in"
-    
+    signin_btn.style.backgroundColor="blue"
+    div_name.style.display = "none";
+        signup_btn.style.display = "none";
+        div_user.style.display="none";
+        div_date.style.display="none"
+        
     })
 signup_btn.addEventListener("click",function(){
-
+       signup_btn.style.backgroundColor="blue"
         title.innerHTML="sign up"
         })
 
