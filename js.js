9d0d7b5b8bef8ec4edecc3af27d4a04772e4cae8
muhiedeen <muhiedeen.alhindi@gmail.com>
2023-02-26 01:08:03 +0200

    add jason object

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-window.onload=function(){
+ 
  let signin_btn=document.getElementById("sign-in")
  let signup_btn=document.getElementById("sign-up")
  let title=document.getElementById("title")
@@ -17,9 +17,10 @@ window.onload=function(){
     title.innerHTML="sign in"
     signin_btn.style.backgroundColor="blue"
     div_name.style.display = "none";
-        signup_btn.style.display = "none";
+        signup_btn.innerHTML = "sign in";
         div_user.style.display="none";
         div_date.style.display="none"
+        signin_btn.style.display="none"
         
         
     })
@@ -37,6 +38,7 @@ signup_btn.addEventListener("click",function(){
                 alert("password should  minimum 8  character long")
             }else if (!/^[A-Z]/.test(password.value)) {
                 alert("Password must start with an uppercase letter.");
+                signup_btn.disabled = true;
               }
         
         })
@@ -44,16 +46,45 @@ signup_btn.addEventListener("click",function(){
             
             if(!/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/.test(email.value)){
                         alert("use like this form muhie@live.com ")
+                        // signup_btn.disabled = true;
                      }
         
         })  
                  
                       
         
+        
+    
+        const myForm = document.getElementById('myform');
+
+        myForm.addEventListener("submit", function(event) {
+          event.preventDefault(); // prevent form submission
           
+          // get form data
+          let name = document.getElementById("name").value;
+          let username = document.getElementById("username").value;
+          let email = document.getElementById("email").value;
+          let password = document.getElementById("password").value;
+          let date = document.getElementById("date").value;
+        
+          // create JSON object
+          let formData = {
+            "name": name,
+            "username": username,
+            "email": email,
+            "password": password,
+            "date": date
+          };
+          
+          console.log(formData); // display JSON object in console
+        });
+        
+       
+        
+ 
      
          
             
-          }
+       
  
 
